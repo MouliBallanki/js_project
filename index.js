47335f951c9b7cc23b4c0cc68f3e70cb93cdd10b
mouli <mouliballanki143@gmail.com>
2021-06-23 17:37:30 +0530

    deleting task completed

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const newCard = ({
     }) =>`<div class="col-md-6 col-lg-4 id=${id}">
     <div class="card">
       <div class="card-header d-flex justify-content-end gap-2">
-        <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
-        <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+        <button type="button"  class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
+        <button type="button" class="btn btn-outline-danger" id =${id} onclick="deleteCard.apply(this , arguments)"><i class="fas fa-trash-alt" id =${id} onclick="deleteCard.apply(this , arguments)"></i></button>
       </div>
 
       <img src=${imageUrl} class=""card-image-top alt="..."/>
@@ -25,7 +25,7 @@ const newCard = ({
     </div>
   </div>`;
 
-const globalStore = [];
+let globalStore = [];
 
 const loadInitialTaskCards = () =>{
   // accessing local storage
@@ -47,6 +47,10 @@ const loadInitialTaskCards = () =>{
   })
 }
 
+const updateLocalData = () =>{
+  localStorage.setItem("tasky",JSON.stringify({cards : globalStore}));
+}
+
 const saveChanges = () =>{
     const taskData = {
         id : `${Date.now()}`, //unique number
@@ -60,9 +64,62 @@ const saveChanges = () =>{
     taskContainer.insertAdjacentHTML("beforeend", createNewCard);
     globalStore.push(taskData) 
     // console.log(globalStore);
-
+    updateLocalData();
     // application programming interface
-    localStorage.setItem("tasky",JSON.stringify({cards : globalStore})); //adding data to local storage
+     //adding data to local storage
 
      
 };
+
+
+const deleteCard = (event) =>{
+  
+  event = window.event;
+
+  // first search the id  and if that id matches in the global store then remove the item
+  const targetId = event.target.id;
+  const tagname = event.target.tagName;//BUTTON
+  
+
+  const newUpdatedArray = globalStore.filter(
+    (cardObject) => cardObject.id !=targetId
+  );
+
+          // then loop over the updated global store and inject the updated card
+  // newUpdatedArray.map((cardObject) =>{
+  //   const createNewCard = newCard(cardObject);
+  //   taskContainer.insertAdjacentHTML("beforeend", createNewCard);
+  // }); this code doesnt work this js it will work only in react and nodejs
+
+  globalStore = newUpdatedArray;
+  updateLocalData();
+
+
+  // access DOM to remove them
+
+  if (tagname === "BUTTON" ){
+
+    // access task_container
+    // event.target.parentNode.parentNode.parentNode.parentNode is nothing but task__conatiner
+    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+
+  }
+
+  return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+
+};
+
+
+
+// Features
+
+// Edit -> Required
+
+// HINT
+
+// Complicated yet easy -> 
+
+// 1. contenteditable
+// 2. setAttributeNode()
+
+// Open (Optional)
